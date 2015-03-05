ee608895bffdad0f03f34cd7c1c22510c518f33d
Mark Kockerbeck <mark@kockerbeck.com>
2015-03-05 07:23:46 -0800

    Trying another travis-ci build script

--- build.sh
+++ build.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-go get -a github.com/google/protobuf
-cd /home/travis/gopath/src/github.com/google/protobuf
-./autogen.sh
-./configure
-make 
-make check
-make install
-go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
-cd $GOPATH/src/github.com/xeb/gload
+
+# install protoc if necessary
+hash protoc &> /dev/null
+if [ $? -eq 1 ]; then
+	go get -a github.com/google/protobuf
+	cd /home/travis/gopath/src/github.com/google/protobuf
+	./autogen.sh
+	./configure --prefix=/usr
+	make 
+	make check
+	make install
+	go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+	cd /home/travis/gopath/src/github.com/xeb/gload
+fi
 
 cd src/
 mkdir -p time
 mkdir -p httpio
+
+# We probably dont need to go through the headache of generating .pb.go's each time, but it seems worth it -- assuming it is possible
+
 protoc -I ./protos ./protos/time.proto --go_out=plugins=grpc:time
 protoc -I ./protos ./protos/httpio.proto --go_out=plugins=grpc:httpio
 go build -o=../bin/agent agent/main.go
