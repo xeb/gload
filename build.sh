5d68754a4a50f3989cc12bc3f35b7adc4e47936b
Mark Kockerbeck <mark@kockerbeck.com>
2015-03-07 22:21:10 -0800

    Travis does a go get by default and thus doesnt like the exec directory

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-go build -o=./bin/agent src/execs/agent.go
-go build -o=./bin/boss src/execs/boss.go
-go build -o=./bin/proxy src/execs/proxy.go
+go build -o=./bin/agent src/agent.go
+go build -o=./bin/boss src/boss.go
+go build -o=./bin/proxy src/proxy.go
