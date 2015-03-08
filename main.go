54a49a1fbb0dae65818cea12dd146b4b230884d3
Mark Kockerbeck <mark@kockerbeck.com>
2015-03-08 12:43:43 -0700

    Adding main for travis

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  //agent "github.com/xeb/gload/src/agent"
+  //boss "github.com/xeb/gload/src/boss"
+  //proxy "github.com/xeb/gload/src/proxy"
+  "fmt"
+)
+
+func main() {
+  fmt.Println("Starting")
+}
