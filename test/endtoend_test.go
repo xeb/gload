8f601502ab1b1371aba6778f4edf73e7d5f807d5
Mark Kockerbeck <mark@kockerbeck.com>
2015-03-07 22:03:20 -0800

    Attempting actual test

--- test/endtoend_test.go
+++ test/endtoend_test.go
@@ -3,16 +3,30 @@ package gload
 import (
 	agent "github.com/xeb/gload/src/agent"
 	boss "github.com/xeb/gload/src/boss"
+	proxy "github.com/xeb/gload/src/proxy"
 	"testing"
 )
 
-var agentadd string = "ipc://agent.gload.test"
-var bossadd string = "ipc://boss.gload.test"
+var agentadd string = "tcp://*:50111"
+var bossadd string = "tcp://*:50112"
 
 func TestEndToEnd(t *testing.T) {
-	agent.Subscribe(agentadd)
-	agent.Close()
 
+	t.Log("Proxy is Binding Agent")
+	proxy.BindAgent(agentadd)
+
+	t.Log("Proxy is Binding Boss")
+	proxy.BindBoss(bossadd)
+
+	t.Log("Agent is Subscribing")
+	agent.Subscribe(agentadd)
+
+	t.Log("Boss is Connecting")
 	boss.Connect(bossadd)
+
+	t.Log("Boss is Sending Command")
 	boss.SendCommand("TEST")
+
+	t.Log("Closing Agent")
+	agent.Close()
 }
